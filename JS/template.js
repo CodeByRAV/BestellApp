97cace6d0986c21f0a0a0e56ecb9ce86e68b6876
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-05-27 13:59:35 +0200

    feat: Added getArchiveNoteTemplate

--- JS/template.js
+++ JS/template.js
@@ -5,3 +5,14 @@ function getNoteTemplate(indexNote) {
 function getTrashNoteTemplate(trashIndex) {
     return `<p>+ T:${trashNotes[trashIndex]} : ${trashNotes[trashIndex]}<button onclick="deleteNote(${trashIndex});">X</button</p>`;
 }
+
+function getArchivedNoteTemplate(indexArchiveNote)
+return `
+<div class="note">
+    <h3>${archiveNotesTitles[indexArchiveNote]}</h3>
+    <p>${archiveNotes[indexArchiveNote]}</p>
+    <div>
+    <button onclick="archiveToTrash(${indexArchiveNote})" class="btn">X</button>
+    <button onclick="archiveToNote(${indexArchiveNote})" class="btn">N</button>
+    </div>
+`
