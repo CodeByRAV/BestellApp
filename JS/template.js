f6b22edd783238e3457ed33e6a38c1784f9d471f
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-05-27 18:28:25 +0200

    fix: buttons were missing ">" in template

--- JS/template.js
+++ JS/template.js
@@ -4,8 +4,8 @@ function getNoteTemplate(indexNote) {
         <h3> ${notesTitles[indexNote]}</h3>
         <p>${notes[indexNote]}</p>
         <div>
-            <button onclick="pushNoteToTrash(${indexNote}); class="btn"">X</button
-            <button onclick="pushNoteToArchive(${indexNote}); class="btn"">A</button
+            <button onclick="pushNoteToTrash(${indexNote});" class="btn">X</button> 
+            <button onclick="pushNoteToArchive(${indexNote});" class="btn">A</button>
         </div>
     </div>`;
 }
@@ -16,8 +16,8 @@ function getTrashNoteTemplate(trashIndex) {
         <h3>${trashNotesTitles[trashIndex]}</h3>
         <p>${trashNotes[trashIndex]}</p>
         <div>
-            <button onclick="deleteNote(${trashIndex}); class="btn"">X</button
-            <button onclick="pushNoteToArchive(${indexNote}); class="btn"">A</button
+            <button onclick="deleteNote(${trashIndex});" class="btn">X</button>
+            <button onclick="pushNoteToArchive(${indexNote});" class="btn">A</button>
         </div>
     </div>`;
 }
