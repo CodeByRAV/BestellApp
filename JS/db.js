29590defbd259e54358ad51ef200b2e918bb8429
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-06-22 18:05:23 +0200

    feat: added menu to the database

---
+++ JS/db.js
@@ -0,0 +1,95 @@
+const menu = [
+    {
+        category: "Burgers",
+        icon: "./assets/icons/burgerIcon.png",
+        description: "Die besten Burger der Welt", 
+        dishes:[
+            {
+                imageSource: "./assets/img/1-veggie-mushroom-black-burger.png",
+                name: "Veggie mushroom black burger",
+                ingredients: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
+                price: 16.90
+            }, 
+            {
+                imageSource: "./assets/img/2-all-meat-burger.png",
+                name: "All Meat burger",
+                ingredients: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ Sauce",
+                price: 15.90
+            },
+            {
+                imageSource: "./assets/img/3-beef-red-burger.png",
+                name: "Beef red burger",
+                ingredients: "Beef, Cheese, Tomatoes, Lettuce, Onion",
+                price: 14.90
+            },
+            {
+                imageSource: "./assets/img/4-big-chicken-burger.png",
+                name: "Big chicken burger",
+                ingredients: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
+                price: 14.90
+            }
+        ]
+    },  
+    {
+        category: "Pizza",
+        icon: "./assets/icons/pizzaIcon.png",
+        description: "Die besten Pizzen der Welt", 
+        dishes:[
+            {
+                imageSource: "./assets/img/1-pizza-margherita.png",
+                name: "Pizza Margherita",
+                ingredients: "Tomato slices, Mozzarella, Basil",
+                price: 11.90
+            },
+            {
+                imageSource: "./assets/img/2-pizza-chorizo.png",
+                name: "Pizza Chorizo",
+                ingredients: "Tomato slices, Mozzarella, Chorizo",
+                price: 13.90
+            },
+            {
+                imageSource: "./assets/img/3-pizza-funghi.png",
+                name: "Pizza Funghi",
+                ingredients: "Red Onion, Olives, Button Mushrooms, Mozzarella",
+                price: 12.90
+            },
+            {
+                imageSource: "./assets/img/4-pizza-formaggi.png",
+                name: "Quattro Formaggi with Chicken",
+                ingredients: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
+                price: 15.90
+            },
+        ]
+    },   
+    {
+            category: "Salad",
+            icon: "./assets/icons/saladIcon.png",
+            description: "Die besten Salate der Welt",
+            dishes:[
+                {
+                    imageSource: "./assets/img/1-warm-beef-arugula-salad.png",
+                    name: "Warm Beef Arugula Salad",
+                    ingredients: "Beef, Arugula, Cherry tomatoes, Parmesan cheese, balsamic vinaigrette",
+                    price: 12.90
+                },
+                {
+                    imageSource: "./assets/img/2-mini-green-salad.png",
+                    name: "Mini Green Salad",
+                    ingredients: "Green salad, Cucumbers, Carrots, Parsely, Radishes, Oregano",
+                    price: 11.90
+                },
+                {
+                    imageSource: "./assets/img/3-green-salad-sea-food.png",
+                    name: "Green Salad with Sea Food",
+                    ingredients: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
+                    price: 13.90
+                }
+                ,{
+                    imageSource: "./assets/img/4-vegan-green-salad-with-tofu.png",
+                    name: "Vegan Green Salad with Tofu",
+                    ingredients: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
+                    price: 10.90
+                }
+            ]
+        }
+];
