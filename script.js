ec621f20f268c6a49be7d2bb9c82a7023fe80fc8
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-06-26 21:14:37 +0200

    feat: added renderBasket function

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
+let basket = [];
+
 function init() {
     renderCategories();
+    renderBasket();
 }
 
 function renderCategories() {
@@ -7,5 +10,17 @@ function renderCategories() {
     for(let indexMenu = 0; indexMenu < menu.length; indexMenu++) {
        const menuCategory = menu[indexMenu];
         menuContainer.innerHTML += templateCategory(menuCategory, indexMenu);
+    }
+}
+
+function renderBasket() {
+    let basketContainer = document.getElementById("basket");
+    basketContainer.innerHTML = "";
+    let totalPrice = 0;
+    for (let indexBasket = 0; indexBasket < basket.length; indexBasket++) {
+        const basketItem = basket[indexBasket];
+        totalPrice += basketItem.price;
+
+        basketContainer.innerHTML += templateBasketItem(basketItem);
     }
 }
