cee031f6baa848bdd19de6a1ac5925495b8be005
CodeByRAV <259128081+CodByRAV@users.noreply.github.com>
2026-06-27 18:09:24 +0200

    feat: added the key "amount" to db.js

--- JS/db.js
+++ JS/db.js
@@ -8,25 +8,29 @@ const menu = [
                 imageSource: "./assets/img/1-veggie-mushroom-black-burger.png",
                 name: "Veggie mushroom black burger",
                 ingredients: "Mixed green salad, Tomatoes, Edamame, Mushrooms",
-                price: 16.90
+                price: 16.90,
+                amount: 0
             }, 
             {
                 imageSource: "./assets/img/2-all-meat-burger.png",
                 name: "All Meat burger",
                 ingredients: "Beef, Bacon, Dill pickles, Smoked cheese, Ketchup, BBQ Sauce",
-                price: 15.90
+                price: 15.90,
+                amount: 0
             },
             {
                 imageSource: "./assets/img/3-beef-red-burger.png",
                 name: "Beef red burger",
                 ingredients: "Beef, Cheese, Tomatoes, Lettuce, Onion",
-                price: 14.90
+                price: 14.90,
+                amount: 0
             },
             {
                 imageSource: "./assets/img/4-big-chicken-burger.png",
                 name: "Big chicken burger",
                 ingredients: "Chicken, Cheese, Tomatoes, Lettuce, Onion, Bell pepper, ",
-                price: 14.90
+                price: 14.90,
+                amount: 0
             }
         ]
     },  
@@ -39,25 +43,29 @@ const menu = [
                 imageSource: "./assets/img/1-pizza-margherita.png",
                 name: "Pizza Margherita",
                 ingredients: "Tomato slices, Mozzarella, Basil",
-                price: 11.90
+                price: 11.90,
+                amount: 0
             },
             {
                 imageSource: "./assets/img/2-pizza-chorizo.png",
                 name: "Pizza Chorizo",
                 ingredients: "Tomato slices, Mozzarella, Chorizo",
-                price: 13.90
+                price: 13.90,
+                amount: 0
             },
             {
                 imageSource: "./assets/img/3-pizza-funghi.png",
                 name: "Pizza Funghi",
                 ingredients: "Red Onion, Olives, Button Mushrooms, Mozzarella",
-                price: 12.90
+                price: 12.90,
+                amount: 0
             },
             {
                 imageSource: "./assets/img/4-pizza-formaggi.png",
                 name: "Quattro Formaggi with Chicken",
                 ingredients: "Chicken, Mozzarella, Gorgonzola, Fontina, Parmigiano Reggiano",
-                price: 15.90
+                price: 15.90,
+                amount: 0
             },
         ]
     },   
@@ -70,25 +78,29 @@ const menu = [
                     imageSource: "./assets/img/1-warm-beef-arugula-salad.png",
                     name: "Warm Beef Arugula Salad",
                     ingredients: "Beef, Arugula, Cherry tomatoes, Parmesan cheese, balsamic vinaigrette",
-                    price: 12.90
+                    price: 12.90,
+                    amount: 0
                 },
                 {
                     imageSource: "./assets/img/2-mini-green-salad.png",
                     name: "Mini Green Salad",
                     ingredients: "Green salad, Cucumbers, Carrots, Parsely, Radishes, Oregano",
-                    price: 11.90
+                    price: 11.90,
+                    amount: 0
                 },
                 {
                     imageSource: "./assets/img/3-green-salad-with-seafood.png",
                     name: "Green Salad with Sea Food",
                     ingredients: "Mixed greens, Cherry tomatoes, Red onion, Mussels, Squid rings, Shrimp, Dijon mustard-lemon dressing with dill",
-                    price: 13.90
+                    price: 13.90,
+                    amount: 0
                 }
                 ,{
                     imageSource: "./assets/img/4-vegan-green-salad-with-tofu.png",
                     name: "Vegan Green Salad with Tofu",
                     ingredients: "Green salad, Cherry tomatoes, Cucumber, Baby spinach, Edamame, Radishes, Bittercress, Tofu, Peanuts",
-                    price: 10.90
+                    price: 10.90,
+                    amount: 0
                 }
             ]
         }
